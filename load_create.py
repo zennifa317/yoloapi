dc7411d792d6944bff93a9c7a2805d953380397d
zenifa317 <zenifa2001317@gmail.com>
2024-11-05 16:10:18 +0900

    initial commit

---
+++ load_create.py
@@ -0,0 +1,24 @@
+import argparse
+import glob
+
+from yolo_api import *
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--img_file', type=str, default=None)
+    parser.add_argument('--txt_file', type=str, default=None)
+    parser.add_argument('--json_path', type=str, default=None)
+
+    arg = parser.parse_args()
+
+    yolo = Yolo()
+
+    img_file = arg.img_file
+    txt_file = arg.txt_file
+    output = arg.json_path
+    img_path = glob.glob(os.path.join(img_file, '*.jpg'))
+    txt_path = glob.glob(os.path.join(txt_file, '*.txt'))
+
+    yolo.load_imgs_annos(images_file=img_path, annotations_file=txt_path)
+    yolo.create_json(json_path=output)
